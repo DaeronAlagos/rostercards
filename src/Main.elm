fb7b71f6d008c81dfef0d62ed8b64e480930a24f
James Curtis <mybastion@gmail.com>
2023-09-09 10:52:25 +0100

    [REF] Group sections in file

--- src/Main.elm
+++ src/Main.elm
@@ -17,10 +17,22 @@ import Zip exposing (Zip)
 import Zip.Entry as Entry exposing (Entry)
 
 
-type Status
-    = New
-    | Loaded String String
-    | Error String
+
+-- MAIN
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = always ( { roster = NoFile, gameSystem = Nothing, status = New }, Cmd.none )
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+
+-- MODEL
 
 
 type alias Model =
@@ -28,6 +40,12 @@ type alias Model =
     , gameSystem : Maybe GameSystem
     , status : Status
     }
+
+
+type Status
+    = New
+    | Loaded String String
+    | Error String
 
 
 type Roster
@@ -77,38 +95,8 @@ subscriptions _ =
     receiveFragment Recv
 
 
-gameSystemAttribute : String
-gameSystemAttribute =
-    "gameSystemName"
 
-
-isNameChar : Char -> Bool
-isNameChar char =
-    Char.isAlpha char || char == ' '
-
-
-nameParser : Parser String
-nameParser =
-    getChompedString <|
-        succeed ()
-            |. chompWhile isNameChar
-
-
-systemParser : Parser GameSystem
-systemParser =
-    succeed GameSystem
-        |. chompUntil gameSystemAttribute
-        |. keyword gameSystemAttribute
-        |. symbol "="
-        |. token "\""
-        |= nameParser
-
-
-parseGameSystem : String -> Maybe GameSystem
-parseGameSystem content =
-    content
-        |> run systemParser
-        |> Result.toMaybe
+-- UPDATE
 
 
 type Msg
@@ -247,6 +235,40 @@ update msg model =
             ( model, sendPrint () )
 
 
+parseGameSystem : String -> Maybe GameSystem
+parseGameSystem content =
+    content
+        |> run systemParser
+        |> Result.toMaybe
+
+
+gameSystemAttribute : String
+gameSystemAttribute =
+    "gameSystemName"
+
+
+isNameChar : Char -> Bool
+isNameChar char =
+    Char.isAlpha char || char == ' '
+
+
+nameParser : Parser String
+nameParser =
+    getChompedString <|
+        succeed ()
+            |. chompWhile isNameChar
+
+
+systemParser : Parser GameSystem
+systemParser =
+    succeed GameSystem
+        |. chompUntil gameSystemAttribute
+        |. keyword gameSystemAttribute
+        |. symbol "="
+        |. token "\""
+        |= nameParser
+
+
 snakeCaseSystemName : Maybe GameSystem -> String
 snakeCaseSystemName name =
     case name of
@@ -257,81 +279,8 @@ snakeCaseSystemName name =
             ""
 
 
-viewGameSystemName : Maybe GameSystem -> String
-viewGameSystemName name =
-    case name of
-        Just game ->
-            game.name
 
-        Nothing ->
-            ""
-
-
-viewTextHtml : String -> List (Html.Html msg)
-viewTextHtml text =
-    case Html.Parser.run text of
-        Ok nodes ->
-            Html.Parser.Util.toVirtualDom nodes
-
-        Err _ ->
-            []
-
-
-type ConditionalClass
-    = Hidden
-    | Flex
-    | None
-
-
-viewUsageStep : String -> String -> String -> ConditionalClass -> Html msg
-viewUsageStep number title description conditional =
-    div
-        [ class "flex grow bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]"
-        , classList [ ( "lg:hidden", conditional == Hidden ), ( "flex", conditional == Hidden ), ( "lg:flex", conditional == Flex ), ( "hidden", conditional == Flex ) ]
-        ]
-        [ div [ class "mr-4 flex w-16 lg:w-12 items-center justify-center bg-gray-800 px-4 text-8xl lg:text-5xl text-white" ]
-            [ span [] [ text number ] ]
-        , div []
-            [ h5 [ class "mb-2 text-7xl lg:text-3xl font-medium leading-tight text-neutral-800" ]
-                [ text title ]
-            , p [ class "mb-4 text-5xl lg:text-2xl text-neutral-600" ]
-                [ text description ]
-            ]
-        ]
-
-
-viewIntro : Html msg
-viewIntro =
-    div
-        [ id "start"
-        , class "flex flex-col gap-4"
-        ]
-        [ div
-            [ id "how"
-            , class "flex flex-wrap gap-4"
-            ]
-            [ viewUsageStep "1" "Upload your Roster" "Choose File' button above and select your Roster file (.ros or .rosz)" None
-            , viewUsageStep "2" "View your Cards" "The game system style sheets are formatted so you can easily view cards for the units on your mobile device." Hidden
-            , viewUsageStep "2" "Print your Cards" "Click Print for a printer friendly version of the unit cards." Flex
-            ]
-        , div [ id "systems", class "bg-white p-12 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]" ]
-            [ h5 [ class "mb-6 text-7xl lg:text-3xl font-medium leading-tight text-neutral-800" ]
-                [ text "Supported Games" ]
-            , ul [ class "list-inside list-disc text-5xl lg:text-2xl" ]
-                [ li [] [ text "Stargrave" ]
-                , li [] [ text "Xenos Rampant" ]
-                ]
-            ]
-        ]
-
-
-viewError : String -> Html Msg
-viewError errorMessage =
-    div
-        [ id "error"
-        , class "mb-6 bg-red-800 p-6 text-4xl text-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] lg:text-xl"
-        ]
-        [ text errorMessage ]
+-- VIEW
 
 
 view : Model -> Html Msg
@@ -393,11 +342,78 @@ view model =
         ]
 
 
-main : Program () Model Msg
-main =
-    Browser.element
-        { init = always ( { roster = NoFile, gameSystem = Nothing, status = New }, Cmd.none )
-        , view = view
-        , update = update
-        , subscriptions = subscriptions
-        }
+viewGameSystemName : Maybe GameSystem -> String
+viewGameSystemName name =
+    case name of
+        Just game ->
+            game.name
+
+        Nothing ->
+            ""
+
+
+viewTextHtml : String -> List (Html.Html msg)
+viewTextHtml text =
+    case Html.Parser.run text of
+        Ok nodes ->
+            Html.Parser.Util.toVirtualDom nodes
+
+        Err _ ->
+            []
+
+
+type ConditionalClass
+    = Hidden
+    | Flex
+    | None
+
+
+viewUsageStep : String -> String -> String -> ConditionalClass -> Html msg
+viewUsageStep number title description conditional =
+    div
+        [ class "flex grow bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]"
+        , classList [ ( "lg:hidden", conditional == Hidden ), ( "flex", conditional == Hidden ), ( "lg:flex", conditional == Flex ), ( "hidden", conditional == Flex ) ]
+        ]
+        [ div [ class "mr-4 flex w-16 lg:w-12 items-center justify-center bg-gray-800 px-4 text-8xl lg:text-5xl text-white" ]
+            [ span [] [ text number ] ]
+        , div []
+            [ h5 [ class "mb-2 text-7xl lg:text-3xl font-medium leading-tight text-neutral-800" ]
+                [ text title ]
+            , p [ class "mb-4 text-5xl lg:text-2xl text-neutral-600" ]
+                [ text description ]
+            ]
+        ]
+
+
+viewIntro : Html msg
+viewIntro =
+    div
+        [ id "start"
+        , class "flex flex-col gap-4"
+        ]
+        [ div
+            [ id "how"
+            , class "flex flex-wrap gap-4"
+            ]
+            [ viewUsageStep "1" "Upload your Roster" "Click 'Select Roster' button above and choose your Roster file (.ros or .rosz)" None
+            , viewUsageStep "2" "View your Cards" "The game system style sheets are formatted so you can easily view cards for the units on your mobile device." Hidden
+            , viewUsageStep "2" "Print your Cards" "Click Print for a printer friendly version of the unit cards." Flex
+            ]
+        , div [ id "systems", class "bg-white p-12 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]" ]
+            [ h5 [ class "mb-6 text-7xl lg:text-3xl font-medium leading-tight text-neutral-800" ]
+                [ text "Supported Games" ]
+            , ul [ class "list-inside list-disc text-5xl lg:text-2xl" ]
+                [ li [] [ text "Stargrave" ]
+                , li [] [ text "Xenos Rampant" ]
+                ]
+            ]
+        ]
+
+
+viewError : String -> Html Msg
+viewError errorMessage =
+    div
+        [ id "error"
+        , class "mb-6 bg-red-800 p-6 text-4xl text-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] lg:text-xl"
+        ]
+        [ text errorMessage ]
